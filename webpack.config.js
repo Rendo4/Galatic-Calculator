ae6db9b1a1975cafb9f969a801e5bb211b55656a
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-07-13 19:54:17 -0700

    added webpack and depedencies. set up blank directories

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Ping Pong',
+      title: 'Galatic Calculator',
       template: './src/index.html',
       inject: 'body'
     })
